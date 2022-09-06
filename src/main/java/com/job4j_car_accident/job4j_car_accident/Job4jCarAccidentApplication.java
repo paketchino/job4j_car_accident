2ac0805a8b26db6a89ecd21ccdadace28e990ebd
paketchino <chansgorn@gmail.com>
2022-09-06 22:24:15 +0300

    initial commit

---
+++ src/main/java/com/job4j_car_accident/job4j_car_accident/Job4jCarAccidentApplication.java
@@ -0,0 +1,13 @@
+package com.job4j_car_accident.job4j_car_accident;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Job4jCarAccidentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Job4jCarAccidentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/job4j_car_accident/job4j_car_accident/Job4jCarAccidentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.job4j_car_accident.job4j_car_accident;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Job4jCarAccidentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
