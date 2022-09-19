53a95013993b4d2a55a462d1dd4306c165afaed2
paketchino <chansgorn@gmail.com>
2022-09-19 23:38:37 +0300

    3. @ModelAttribute. Создание инцидента. [#261013]

--- src/main/java/com/jobjcaraccident/controller/AccidentController.java
+++ src/main/java/com/jobjcaraccident/controller/AccidentController.java
@@ -2,26 +2,32 @@ package com.jobjcaraccident.controller;
 
 import com.jobjcaraccident.model.Accident;
 import com.jobjcaraccident.service.AccidentService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+@Controller
 public class AccidentController {
-
     private final AccidentService accidentService;
 
     public AccidentController(AccidentService accidentService) {
         this.accidentService = accidentService;
     }
 
-    /*
-    Get
-     */
+    @GetMapping("/createAccident")
     public String addAccident() {
-        return "/addAccident";
+        return "/createAccident";
     }
 
-    /*
-    Post
-     */
-    public String createAccident(Accident accident) {
+   @PostMapping("/saveAccident")
+    public String saveAccident(@ModelAttribute Accident accident,
+                               @RequestParam("file") MultipartFile file)
+           throws Exception {
+        accident.setPhoto(file.getBytes());
+        accidentService.add(accident);
         return "redirect:/index";
     }
 }
