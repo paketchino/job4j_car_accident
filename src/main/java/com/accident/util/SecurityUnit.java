173988ccae1065acb8232dccc7e1c66559a94ddd
paketchino <chansgorn@gmail.com>
2022-10-05 00:54:05 +0300

    0. Spring Security [#6879]

---
+++ src/main/java/com/accident/util/SecurityUnit.java
@@ -0,0 +1,6 @@
+package com.accident.util;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityUnit extends AbstractSecurityWebApplicationInitializer {
+}
