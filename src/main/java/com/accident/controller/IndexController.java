a9cc30fd2366015dfddc58684dc0ff5da6545cf2
paketchino <chansgorn@gmail.com>
2022-10-06 00:04:47 +0300

    1. Авторизация через JDBC [#2094]

--- src/main/java/com/accident/controller/IndexController.java
+++ src/main/java/com/accident/controller/IndexController.java
@@ -7,11 +7,13 @@ import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.security.Security;
 import java.util.Optional;
 
 @Controller
@@ -25,6 +27,8 @@ public class IndexController {
 
     @GetMapping("/index")
     public String index(Model model) {
+        model.addAttribute("user", SecurityContextHolder.getContext()
+                .getAuthentication().getPrincipal());
         model.addAttribute("accidents", accidentService.getAll());
         return "/index";
     }
